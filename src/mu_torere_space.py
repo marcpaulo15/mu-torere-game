41ae7bf37da5c06369a7c3d6d1c9090558cdc720
marcpaulo15 <marc15paulo07@gmail.com>
2023-02-13 15:14:02 +0100

    refactoring: basic unit is MuTorereSpace

--- src/mu_torere_space.py
+++ src/mu_torere_space.py
@@ -3,41 +3,38 @@ import json
 from typing import Tuple
 
 
-class MuTorereSlot:
+class MuTorereSpace:
     """
-    MuTorereSlot class defines the logic behind a slot in a Mu Torere board.
-    There are 9 slots in a simple board. Each player has 4 counters that
-    are placed in different slots, so at any step there is only one empty slot.
-    A counter (slot filled by any of the player) is available if the active
-    player is allowed to move it to the emtpy slot.
+    MuTorereSpace class defines the logic behind a space in a Mu Torere board.
+    There are 9 spaces in a simple board. Each player has 4 counters that
+    are placed in different spaces, so at any step there is only one empty
+    space. A counter (space filled by any of the player) is available when the
+    active player is allowed to move it to the emtpy space.
 
-    MuTorereSlot Attributes:
-        - id_:           slot identifier, number from 0 to 8
-        - center:        (x,y) coordinates where the slot is placed
+    MuTorereSpace Attributes:
+        - center:        (x,y) coordinates where the space is centered
         - state:         0 if it's empty, 1|2 if any player's counter is placed
         - is_available:  whether the counter can be chosen in current turn
         - rect:          allows to detect collisions with mouse clicks
 
-    MuTorereSlot Methods:
+    MuTorereSpace Methods:
         - update:               updates the value of 'state' attribute
         - update_availability:  updates the value of 'is_available' attribute
-        - draw:                 displays the slot on the given surface
+        - draw:                 displays the space on the given surface
     """
 
     def __init__(self,
-                 id_: int,
                  center: Tuple[int, int],
                  state: int = 0,
                  is_available: bool = False,
                  config_path: str = "../config/config.json") -> None:
         """
-        Inits a MuTorereSlot instance with a given 'id_'. It is placed at a
-        given position 'center'. If 'state' is 1 or 2, it means that there is
-        a piece placed on that position. If 'state' is 0, it means that it is
-        empty. If 'is_available', the counter can be chosen by active_player
+        Inits a MuTorereSpace instance centered at a given position 'center'.
+        If 'state' is 1 or 2, it means that there is a piece placed on this
+        spoce. If 'state' is 0, it means that it is empty. If 'is_available',
+         the counter (filled space) can be chosen by active_player
 
-        :param id_: slot identifier, number from 0 to 8
-        :param center: (x,y) coordinates where the slot is placed
+        :param center: (x,y) coordinates where the space is placed
         :param state: 0 if it's empty, 1|2 if any player's counter is placed
         :param is_available: whether the counter can be chosen in current turn
         :param config_path: path from where to read the configuration file
@@ -47,11 +44,8 @@ class MuTorereSlot:
 
         # some sanity checks
         if state not in (0, 1, 2):  # meaning: (empty, player1, player2)
-            raise ValueError("wrong state value for MuTorereSlot instance")
-        if id_ not in range(10):
-            raise ValueError("wrong id_ value for MuTorereSlot instance")
+            raise ValueError("wrong state value for MuTorereSpace instance")
 
-        self.id_ = id_  # do not overwrite id() built-in function
         self.center = center
         self.state = state
         self.is_available = is_available  # True: it can be chosen by a player
@@ -89,12 +83,12 @@ class MuTorereSlot:
         if state in (0, 1, 2):
             self.state = state
         else:
-            raise ValueError("wrong state value for MuTorereSlot instance")
+            raise ValueError("wrong state value for MuTorereSpace instance")
 
     def update_availability(self, is_available: bool) -> None:
         """
         Updates the availability of the counter. Availability only matters when
-        the slot is not empty i.e. it represents a counter.
+        the space is not empty i.e. it represents a counter.
 
         :param is_available: new value for 'is_available' attribute
         :return: None
@@ -104,14 +98,14 @@ class MuTorereSlot:
 
     def draw(self, screen: pygame.Surface) -> None:
         """
-        Displays the counter on the given surface. If the slot is empty (state
+        Displays the counter on the given surface. If the space is empty (state
         is zero) do nothing.
 
-        :param screen: pygame surface where the slot is displayed on
+        :param screen: pygame surface where the space is displayed on
         :return: None
         """
 
-        if self.state == 0:  # if the slot is empty, do nothing
+        if self.state == 0:  # if the space is empty, do nothing
             return
 
         if self.is_available:
@@ -135,19 +129,19 @@ if __name__ == "__main__":
     screen_width, screen_height = 800, 800
     pygame.init()
     screen = pygame.display.set_mode((screen_width, screen_height))
-    pygame.display.set_caption("~ MuTorereSlot class. Demo ~")
+    pygame.display.set_caption("~ MuTorereSpace class. Demo ~")
     clock = pygame.time.Clock()
 
-    # 2) Initialize a set of random Slots having different locations and states
+    # 2) Initialize a set of random Spaces having different locations and states
     random_counter_list = [
         # Two counters for player1, one available and the other unavailable
-        MuTorereSlot(id_=0, center=(400, 400), state=1, is_available=True),
-        MuTorereSlot(id_=1, center=(100, 150), state=1, is_available=False),
+        MuTorereSpace(center=(400, 400), state=1, is_available=True),
+        MuTorereSpace(center=(100, 150), state=1, is_available=False),
         # Two counters for player2, one available and the other unavailable
-        MuTorereSlot(id_=2, center=(600, 60),  state=2, is_available=True),
-        MuTorereSlot(id_=3, center=(600, 700), state=2, is_available=False),
+        MuTorereSpace(center=(600, 60),  state=2, is_available=True),
+        MuTorereSpace(center=(600, 700), state=2, is_available=False),
         # One extra empty counter. Since it's empty, it is not drawn
-        MuTorereSlot(id_=4, center=(200, 350), state=0, is_available=False)
+        MuTorereSpace(center=(200, 350), state=0, is_available=False)
     ]
 
     # 3) Set up the usual pygame flow
@@ -159,7 +153,7 @@ if __name__ == "__main__":
                 done = True
 
         screen.fill([255, 255, 255])  # fill the screen with a black background
-        # Display the Slots (counters) on the screen
+        # Display the Space (counters) on the screen
         for counter in random_counter_list:
             counter.draw(screen)
         pygame.display.update()  # update the screen content
